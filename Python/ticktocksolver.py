060d795d5986f16241d831ef74c91b5a2563b3a7
Benjamin Yin <fellfromatree@gmail.com>
2014-11-02 15:03:00 -0800

    added scripts for the tick-tock problem on picoCTF

---
+++ Python/ticktocksolver.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import os
+import sys
+if sys.platform.startswith("win"):
+	import winsound
+
+secretz = [(1, 2), (2, 3), (8, 13), (4, 29), (130, 191), (343, 397), (652, 691), (858, 1009),
+           (689, 2039), (1184, 4099), (2027, 7001), (5119, 10009), (15165, 19997), (15340, 30013),
+           (29303, 70009), (42873, 160009), (158045, 200009)]
+done = []
+def beep():
+	if sys.platform.startswith("linux"):
+		os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 1, 300))	
+	elif sys.platform.startswith("win"):
+		winsound.beep(300,1)
+for i in xrange(157578377,2147483647):
+	works = 1
+	for (r,m) in secretz:
+  		if i%m != r:
+    			#print str(130*i+191) +  "doesn't work for" + str(r) + "," + str(m)
+			works = 0
+			break
+		else:
+			if m not in done:
+				done.append(m)
+				print str(i) + " mod " + str(m) + " is " + str(r) + " and all of the above"
+				beep()
+
+	if works == 1:
+		print str(i) + "works"
+		beep()
+		break
+beep()
